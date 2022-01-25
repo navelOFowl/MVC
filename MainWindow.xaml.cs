11095212fc48142652b4d2ef2710c24428545206
Курников Дмитрий <smilyck9@outlook.com>
2022-01-25 11:08:25 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.tbFrst = tbFrstNum;
+            Model.tbScnd = tbScndNum;
+            Model.tbEqual = tbEqual;
+            cbOper.ItemsSource = Model.operList;
+        }
+
+        private void buttEqual_Click(object sender, RoutedEventArgs e)
+        {
+            Model.frstNum = Convert.ToInt32(tbFrstNum.Text);
+            Model.scndNum = Convert.ToInt32(tbScndNum.Text);
+            Model.cbOper = cbOper.SelectedIndex;
+            tbEqual.Text = Model.Equal;
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MVC
+{
+    static class Model
+    {
+        static int frst = 0;
+        static int scnd = 0;
+        public static List<string> operList = new List<string> { "+", "-", "*", "/" };
+        static int operId = 0;
+
+        public static TextBox tbFrst;
+        public static TextBox tbScnd;
+        public static TextBox tbEqual;
+
+        public static int frstNum
+        {
+            set
+            {
+                frst = value;
+            }
+        }
+        public static int scndNum
+        {
+            set
+            {
+                scnd = value;
+            }
+        }
+        public static int cbOper
+        {
+            set
+            {
+                operId = value;
+            }
+        }
+        public static string Equal
+        {
+            get
+            {
+                switch(operId)
+                {
+                    case 0:
+                        return tbEqual.Text = (frst + scnd).ToString();
+                    case 1:
+                        return  tbEqual.Text = (frst - scnd).ToString();
+                    case 2:
+                        return  tbEqual.Text = (frst * scnd).ToString();
+                    case 3:
+                        return  tbEqual.Text = (frst / scnd).ToString();
+                    default:
+                        return "!!!";
+                }
+            }
+        }
+    }
+}
